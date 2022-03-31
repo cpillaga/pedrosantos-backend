ef7534a5753279eb2a999eee54bdff0c7ccee63d
sandra1997-06 <sandra-14-2012@hotmail.com>
2022-03-31 16:54:13 -0500

    listar direccion por usuario

--- server/routes/direccion.js
+++ server/routes/direccion.js
@@ -4,89 +4,9 @@ let Direccion = require('../models/direccion');
 
 let app = express();
 
-const { verificaToken } = require('../middlewares/autenticacion');
-
-//=====================================
-//mostrar todas las categorias de una empresa
-//=====================================
-
-app.get('/direccion/:usuario', (req, res) => {
-
-    let usuarioB = req.params.usuario;
-
-    Direccion.find({ usuario: usuarioB })
-        .populate('usuario')
-        .exec((err, direccionDB) => {
-            if (err) {
-                return res.status(500).json({
-                    ok: false,
-                    err
-                });
-            }
-            res.json({
-                ok: true,
-                direccionDB
-            });
-        });
-});
-
-//=====================================
-//mostrar una direccion por id.
-//=====================================
-
-app.get('/direccionId/:id', (req, res) => {
-    let id = req.params.id;
-    Direccion.findById(id, (err, direccionDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        }
-
-        if (!direccionDB) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    mensaje: 'el id no existe en la tabla direccion'
-                }
-            });
-        }
-        res.json({
-            ok: true,
-            direccion: direccionDB
-        });
-    });
-});
-
 //=====================================
 //crear nueva direccion
 // //=====================================
-// app.post("/carritos", (req, res) => {
-//     let body = req.body;
-//     let direccion = new Direccion({
-//         principal: body.principal,
-//         secundaria: body.secundaria,
-//         referencia: body.referencia,
-//         lat: body.lat,
-//         lng: body.lng,
-//         usuario: body.usuario
-//     });
-
-//     direccion.save((err, direccionBD) => {
-//         if (err) {
-//             return res.status(500).json({
-//                 ok: false,
-//                 err
-//             });
-//         }        
-//         res.status(201).json({
-//             ok: true,
-//             direccion: direccionBD
-//         });
-//     });
-// });
-
 
 app.post('/direccion', (req, res) => {
     let body = req.body;
@@ -120,56 +40,30 @@ app.post('/direccion', (req, res) => {
     });
 });
 
-
 //=====================================
-//Modifica direccion por ID
-//=====================================
-app.put('/direccion/:id', function(req, res) {
+//Lista direccion por usuario
+// //=====================================
+app.get('/direccion/:id', (req, res) => {
     let id = req.params.id;
-
-    let body = _.pick(req.body, ['principal', 'secundaria', 'referencia', 'lat', 'lng', 'usuario']);
-
-    Direccion.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, direccionDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
-
-        res.json({
-            ok: true,
-            direccion: direccionDB
-        });
-    });
-});
-
-//=====================================
-//eliminar nueva direccion
-//=====================================
-
-app.delete('/direccion/:id', (req, res) => {
-    let id = req.params.id;
-
-    Direccion.findByIdAndRemove(id, (err, direccionBD) => {
+    Direccion.findById(id, (err, direccionBD) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
                 err
             });
-        }
+        };
         if (!direccionBD) {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'el id no existe'
+                    mensaje: 'El id no existe en la tabla direccion'
                 }
-            });
+            })
         }
         res.json({
             ok: true,
-            mensaje: 'direccion borrada'
-        });
+            direccion: direccionBD
+        })
     });
 });
 
